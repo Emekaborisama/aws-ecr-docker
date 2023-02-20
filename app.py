99e15ccb62d8717f8da1a883bffeee90908370f7
Emeka boris ama <borisphilosophy@gmail.com>
2023-02-20 02:14:05 +0100

    Create app.py

---
+++ app.py
@@ -0,0 +1,18 @@
+#  this is a sample rest api script for testing
+
+
+from flask import Flask,jsonify
+
+
+app = Flask(__name__)
+
+
+
+@app.route("/home")
+def index():
+  return("welcome to app")
+
+
+
+if __name__ == '__main__':
+    app.run(host= '0.0.0.0', port = 8000, debug=True)
